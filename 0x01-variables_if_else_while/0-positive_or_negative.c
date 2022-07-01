abeb8224a3226b40d4cacb71d5e6f3743644a5e8
alemayehutseganew <55890810+alemayehutseganew@users.noreply.github.com>
2022-07-01 12:58:09 +0300

    positive or negative intiger

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,21 +4,21 @@
 /* more headers goes there */
 
 /**
- *  * main - Entry point
- *   * Description - Prints if random generated number is +tive, zero or -tive
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ * Description - Prints if random generated number is +tive, zero or -tive
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		int n;
+	int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					if (n < 0)
-								printf("%d is negative\n", n);
-						else if (n == 0)
-									printf("%d is zero\n", n);
-							else
-										printf("%d is positive\n", n);
-								return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+		printf("%d is negative\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is positive\n", n);
+	return (0);
 }
