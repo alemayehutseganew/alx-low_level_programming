0f98efe7a8f41c811209af3285ea3646c3c11075
alemayehutseganew <55890810+alemayehutseganew@users.noreply.github.com>
2022-07-05 19:18:59 +0300

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,36 +1,28 @@
-#include "holberton.h"
-#include "stdio.h"
+#include <stdio.h>
+
 /**
- * print_to_98 - print all the numbers between the provided number and 98
- * @n: integer to start at
- * Return: Void.
+ * print_to_98 - Prints numbers from n to 98.
+ *
+ * @n: Theinput number.
+ *
+ * Return: no return.
  */
 
 void print_to_98(int n)
 {
-	int i = 0;
-
 	if (n > 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (; n > 98; n--)
 		{
-			if (i != n)
-			{
-				printf(", ");
-			}
-			printf("%d", i);
+			printf("%d, ", n);
 		}
 	}
-	else
+	else if (n < 98)
 	{
-		for (i = n; i <= 98 ; i++)
+		for (; n < 98; n++)
 		{
-			if (i != n)
-			{
-				printf(", ");
-			}
-			printf("%d", i);
+			printf("%d, ", n);
 		}
 	}
-	putchar('\n');
+	printf("%d\n", n);
 }
