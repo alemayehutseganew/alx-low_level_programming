ab1e007172827702b4369848332bdef914e52b16
alemayehutseganew <55890810+alemayehutseganew@users.noreply.github.com>
2022-07-05 22:59:28 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
-#include "main.h"
 
 /**
- * print_alphabet - Prints alphabets 'a - z'
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
  */
-
 void print_alphabet(void)
 {
-	char c;
+	int i;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (i = 97; i <= 122; i++)
 	{
-		_putchar(c);
+		_putchar(i);
 	}
 	_putchar('\n');
 }
