8930460d547ce6427dd3443c9e2d9aa13afdd778
alemayehutseganew <55890810+alemayehutseganew@users.noreply.github.com>
2022-07-05 18:49:40 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		_putchar (alp);
+		alp++;
+
+	}
+	_putchar ('\n');
+
+}
