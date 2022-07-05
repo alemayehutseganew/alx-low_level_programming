0892ebb40e63543d3c2f8120c3caa77b57f67508
alemayehutseganew <55890810+alemayehutseganew@users.noreply.github.com>
2022-07-05 18:50:29 +0300

    print alpphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - adds two integers and returns the result
+ * @a: an integer to add to b
+ * @b: an integer to add to a
+ * Return: Void.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
