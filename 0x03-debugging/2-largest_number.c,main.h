dc31dccfcc6b55c5e58e718e860e68cd5104e9d2
alemayehutseganew <55890810+alemayehutseganew@users.noreply.github.com>
2022-07-07 17:15:50 +0300

    Create 2-largest_number.c,main.h

---
+++ 0x03-debugging/2-largest_number.c,main.h
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest = b;
+
+	if (a > b)
+	{
+		largest = a;
+	}
+	else if (c > b)
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
