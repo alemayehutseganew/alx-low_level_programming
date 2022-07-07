bd10c834ebb4b6126c2b3ebe1fc9c938f2023669
alemayehutseganew <55890810+alemayehutseganew@users.noreply.github.com>
2022-07-07 15:51:50 +0300

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - Multiply to integers
+ *
+ * @a: interger one
+ * @b: interger two
+ *
+ * Return: Proudct of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
