7c4b2d29a6c9babe8cfc4350431d6386a45dc818
alemayehutseganew <55890810+alemayehutseganew@users.noreply.github.com>
2022-07-08 09:46:22 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,34 +1,15 @@
 #include "main.h"
 
 /**
- * print_diagonal - entry point
+ * main - check the code
  *
- * Description: Prints diagonals
- *@n: number of spaces
- * Return: void
+ * Return: Always 0.
  */
-
-void print_diagonal(int n)
+int main(void)
 {
-	int row, num_lines;
-
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (row = 1; row <= n; row++)
-		{
-			if (row > 1)
-			{
-				for (num_lines = 1; num_lines <= row - 1; num_lines++)
-				{
-				_putchar(' ');
-				}
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
-	}
+    print_diagonal(0);
+    print_diagonal(2);
+    print_diagonal(10);
+    print_diagonal(-4);
+    return (0);
 }
