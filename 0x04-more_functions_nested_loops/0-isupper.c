d8d9f8139f9dfb87041614d3f95c6f1096612f5f
alemayehutseganew <55890810+alemayehutseganew@users.noreply.github.com>
2022-07-08 09:38:43 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,13 @@
-#include "main.h"
+
 #include <stdio.h>
 
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
+
+int main(){
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
 	else
 		return (0);
+}
 }
